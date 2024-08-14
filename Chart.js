359f906fd1cc1af15ff61f4f00c35c7209cb7da4
Pawelek11 <pawelekklonowski@o2.pl>
2024-08-14 08:43:23 +0200

    Update Chart.js

--- Chart.js
+++ Chart.js
@@ -6,25 +6,14 @@ let playersTeams;
 let players1vs1;
 let players2vs2;
 let currentData;
-let gracze = {};
 let allTimestamps;
 let myLineChart;
 
     fetch('https://api.codetabs.com/v1/proxy?quest=https://bubbleam.pl/players')
         .then(response => response.json())
         .then(data => {
-            //console.log('odp Api :', data);
-            gracze = data;
-           // console.log('gr: ', gracze);
             playersMS1 = data['443'].sort((a, b) => a[0] - b[0]);
-            playersMS2 = data['2053'].sort((a, b) => a[0] - b[0]);
-            playersHC = data['2083'].sort((a, b) => a[0] - b[0]);
-            playersTeams = data['2096'].sort((a, b) => a[0] - b[0]);
-            players1vs1 = data['8443'].sort((a, b) => a[0] - b[0]);
-            players2vs2 = data['2087'].sort((a, b) => a[0] - b[0]);
-            currentData = playersMS1;
-            const colorOfDay = ['red', 'yellow', 'pink', 'orange', 'purple', 'green', 'blue', 'black'];
-
+            while (playersMS1[0] === 0) playersMS1.shift();
             myLineChart = new Chart(ctx, {
                 type: 'line',
                 data: {
@@ -53,8 +42,10 @@ let myLineChart;
                         x: {
                             ticks: {                    //etykiety
                                 maxTicksLimit: 40,
-                               color: (context) => colorOfDay[new Date(context.tick['label']).getDay()]
+                                color: (context) => colorOfDay[new Date(context.tick['label']).getDay()],
+                               
                             },
+                            //type: 'linear'
                         },
                         y: {
                             ticks: {                    //etykiety
@@ -63,7 +54,9 @@ let myLineChart;
                             title: {
                                 display: true,
                                 text: 'Number of Players'
-                            }
+                            },
+                            min: 0
+                           // type: 'linear'
                         }
                     },
                     interaction: {
@@ -75,14 +68,13 @@ let myLineChart;
                         enabled: true
                     },
                     plugins: {
-                        
                         legend: {
                             display: false
                         },
                         zoom: { 
                             pan: {
                                 enabled: true,
-                                mode: 'x'
+                                mode: 'x',
                             },
                             zoom: { 
                                 wheel: { 
@@ -100,6 +92,18 @@ let myLineChart;
                     }
                 }
             });
+            playersMS2 = data['2053'].sort((a, b) => a[0] - b[0]);
+            while (playersMS2[0] === 0) playersMS2.shift();
+            playersHC = data['2083'].sort((a, b) => a[0] - b[0]);
+            while (playersHC[0] === 0) playersHC.shift();
+            playersTeams = data['2096'].sort((a, b) => a[0] - b[0]);
+            while (playersTeams[0] < 5) playersTeams.shift();
+            players1vs1 = data['8443'].sort((a, b) => a[0] - b[0]);
+            while (players1vs1[0] === 0) players1vs1.shift();
+            players2vs2 = data['2087'].sort((a, b) => a[0] - b[0]);
+            while (players2vs2[0] === 0) players2vs2.shift();
+            currentData = playersMS1;
+            const colorOfDay = ['red', 'yellow', 'pink', 'orange', 'purple', 'green', 'blue', 'black'];
         })
     .catch(error => console.error('Błąd pobierania danych:', error));
 
@@ -108,7 +112,6 @@ document.getElementById('MS1').classList.add('active');         //oznaczenie prz
 let hourlyData = {};      //obiekt z godziną i średnią ilością grraczy w tej godzinie
 let Labels2;
 function averagePerHour(numberHourToAverage) {
-    console.log('gracze: ', gracze);
     hourlyData = {};
     let currentHourKey = null;  // godzina dla której obliczamy średnią
     let currentHourPlayers = [];
@@ -142,124 +145,84 @@ function averagePerHour(numberHourToAverage) {
         const date = new Date(hourkey * 3600000)
         return date.toString().slice(0, 21);
     });
-}
+};
 
 const topButtons = document.querySelectorAll('.top-buttons .btn');
 const bottomButtons = document.querySelectorAll('.bottom-buttons .btn');
 
+function topBtnAction(players, activeBtn) {
+    myLineChart.data.labels = players.map(item => new Date(item[0]).toString().slice(0, 21));
+    myLineChart.data.datasets[0].data = players.map(item => item[1]);
+    topButtons.forEach(b => b.classList.remove('active'));
+    bottomButtons.forEach(b => b.classList.remove('active'));
+    myLineChart.resetZoom();
+    myLineChart.update();
+    currentData = players;
+    document.getElementById(activeBtn).classList.add('active');
+};
+
+function bottomBtnAction(numAvgHour, activeBtn) {
+    bottomButtons.forEach(b => b.classList.remove('active'));
+    document.getElementById(activeBtn).classList.add('active');
+    myLineChart.resetZoom();
+    averagePerHour(numAvgHour);
+    myLineChart.data.datasets[0].data = Object.values(hourlyData);
+    myLineChart.data.labels = Labels2;
+    myLineChart.update();
+};
+
 {
     document.getElementById('MS1').addEventListener('click', () => {
-        topButtons.forEach(b => b.classList.remove('active'));
-        bottomButtons.forEach(b => b.classList.remove('active'));
-        document.getElementById('MS1').classList.add('active');
-        myLineChart.data.labels = playersMS1.map(item => new Date(item[0]).toString().slice(0, 21));
-        myLineChart.data.datasets[0].data = playersMS1.map(item => item[1]);
-        myLineChart.update();
-        currentData = playersMS1;
+        topBtnAction(playersMS1, 'MS1');
     });
+
     document.getElementById('MS2').addEventListener('click', () => {
-        topButtons.forEach(b => b.classList.remove('active'));
-        bottomButtons.forEach(b => b.classList.remove('active'));
-        document.getElementById('MS2').classList.add('active');
-        myLineChart.data.labels = playersMS2.map(item => new Date(item[0]).toString().slice(0, 21));
-        myLineChart.data.datasets[0].data = playersMS2.map(item => item[1]);
-        myLineChart.update();
-        currentData = playersMS2;
+        topBtnAction(playersMS2, 'MS2');
     });
+
     document.getElementById('HC').addEventListener('click', () => {
-        topButtons.forEach(b => b.classList.remove('active'));
-        bottomButtons.forEach(b => b.classList.remove('active'));
-        document.getElementById('HC').classList.add('active');
-        myLineChart.data.labels = playersHC.map(item => new Date(item[0]).toString().slice(0, 21));
-        myLineChart.data.datasets[0].data = playersHC.map(item => item[1]);
-        myLineChart.update();
-        currentData = playersHC;
+        topBtnAction(playersHC, 'HC');
     });
+
     document.getElementById('Teams').addEventListener('click', () => {
-        topButtons.forEach(b => b.classList.remove('active'));
-        bottomButtons.forEach(b => b.classList.remove('active'));
-        document.getElementById('Teams').classList.add('active');
-        myLineChart.data.labels = playersTeams.map(item => new Date(item[0]).toString().slice(0, 21));
-        myLineChart.data.datasets[0].data = playersTeams.map(item => item[1]);
-        myLineChart.update();
-        currentData = playersTeams;
+        topBtnAction(playersTeams, 'Teams');
     });
+
     document.getElementById('1vs1').addEventListener('click', () => {
-        topButtons.forEach(b => b.classList.remove('active'));
-        bottomButtons.forEach(b => b.classList.remove('active'));
-        document.getElementById('1vs1').classList.add('active');
-        myLineChart.data.labels = players1vs1.map(item => new Date(item[0]).toString().slice(0, 21));
-        myLineChart.data.datasets[0].data = players1vs1.map(item => item[1]);
-        myLineChart.update();
-        currentData = players1vs1;
+        topBtnAction(players1vs1, '1vs1');
     });
+
     document.getElementById('2vs2').addEventListener('click', () => {
-        topButtons.forEach(b => b.classList.remove('active'));
-        bottomButtons.forEach(b => b.classList.remove('active'));
-        document.getElementById('2vs2').classList.add('active');
-        myLineChart.data.labels = players2vs2.map(item => new Date(item[0]).toString().slice(0, 21));
-        myLineChart.data.datasets[0].data = players2vs2.map(item => item[1]);
-        myLineChart.update();
-        currentData = players2vs2;
+        topBtnAction(players2vs2, '2vs2');
     });
 }       //top-button
 
 {       //bottom-button
     document.getElementById('av1').addEventListener('click', () => {
-       bottomButtons.forEach(b => b.classList.remove('active'));
-       document.getElementById('av1').classList.add('active');
-       averagePerHour(1);
-       myLineChart.data.datasets[0].data = Object.values(hourlyData);
-       myLineChart.data.labels = Labels2;
-       myLineChart.update();
-});
+        bottomBtnAction(1, 'av1');
+    });
 
     document.getElementById('av2').addEventListener('click', () => {
-        bottomButtons.forEach(b => b.classList.remove('active'));
-        document.getElementById('av2').classList.add('active');
-        averagePerHour(2);
-        myLineChart.data.datasets[0].data = Object.values(hourlyData);
-        myLineChart.data.labels = Labels2;
-        myLineChart.update();
-});
+        bottomBtnAction(2, 'av2');
+    });
     document.getElementById('av3').addEventListener('click', () => {
-        bottomButtons.forEach(b => b.classList.remove('active'));
-        document.getElementById('av3').classList.add('active');
-        averagePerHour(3);
-        myLineChart.data.datasets[0].data = Object.values(hourlyData);
-        myLineChart.data.labels = Labels2;
-        myLineChart.update();
+        bottomBtnAction(3, 'av3');
     });
+
     document.getElementById('av4').addEventListener('click', () => {
-        bottomButtons.forEach(b => b.classList.remove('active'));
-        document.getElementById('av4').classList.add('active');
-        averagePerHour(4);
-        myLineChart.data.datasets[0].data = Object.values(hourlyData);
-        myLineChart.data.labels = Labels2;
-        myLineChart.update();
+        bottomBtnAction(4, 'av4');
     });
+
     document.getElementById('av6').addEventListener('click', () => {
-        bottomButtons.forEach(b => b.classList.remove('active'));
-        document.getElementById('av6').classList.add('active');
-        averagePerHour(6);
-        myLineChart.data.datasets[0].data = Object.values(hourlyData);
-        myLineChart.data.labels = Labels2;
-        myLineChart.update();
+        bottomBtnAction(6, 'av6');
     });
+
     document.getElementById('av12').addEventListener('click', () => {
-        bottomButtons.forEach(b => b.classList.remove('active'));
-        document.getElementById('av12').classList.add('active');
-        averagePerHour(12);
-        myLineChart.data.datasets[0].data = Object.values(hourlyData);
-        myLineChart.data.labels = Labels2;
-        myLineChart.update();
+        bottomBtnAction(12, 'av12');
     });
+
     document.getElementById('av24').addEventListener('click', () => {
-        bottomButtons.forEach(b => b.classList.remove('active'));
-        document.getElementById('av24').classList.add('active');
-        averagePerHour(24);
-        myLineChart.data.datasets[0].data = Object.values(hourlyData);
-        myLineChart.data.labels = Labels2;
-        myLineChart.update();
+        bottomBtnAction(24, 'av24');
+
     });
 }     //bottom-button
